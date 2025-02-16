53d20fa16431e9ce71408dc6c21c8a9dfdf32a7a
Bryan Barrientos <49875686+b-barrientos@users.noreply.github.com>
2025-02-16 11:37:40 -0600

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,21 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+function promptUser () {
+// This automatically prompts with the first expected prompt of "$ ${answer}"
 rl.question("$ ", (answer) => {
-  let i = 0;
-  while (answer) {
-  rl.write(`${answer}${i}: command not found`);
-  i++;
-  }
-  rl.close();
+  // handle user input
+  rl.write(`${answer}: command not found\n`);
+
+  /**
+   * Recall the function itself again to cause a REPL loop (Read-evaluate-prompt-loop)
+   *  rl.question gives the first read -> $ ${answer}
+   *  rl.write evaluates and returns -> command not found
+   *  promptUser(); call to itself prompts the user again and handles the loop portion as well
+  */
+  promptUser();
 });
+};
+
+// Invoke the promptUser function at start up. This is where it is called first when spinned up.
+promptUser();
